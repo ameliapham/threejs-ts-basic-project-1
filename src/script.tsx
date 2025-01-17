16fb1e1f4f5cefb53e589140a6705a09d7774a05
ameliapham <ptt.huong.8895@gmail.com>
2025-01-18 00:26:52 +0100

    Set up a basic project's structure

--- src/script.tsx
+++ src/script.tsx
@@ -2,3 +2,28 @@ import * as THREE from 'three'
 import gsap from 'gsap'
 
 console.log("Hello, Three.js with TypeScript!");
+
+// Set up scene
+const scene = new THREE.Scene();
+
+// Set up camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+scene.add(camera);
+
+// Create object
+const geometry = new THREE.BoxGeometry(1,1,1);
+const material = new THREE.MeshBasicMaterial({ color: "pink" });
+
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+// Set up renderer
+const canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement;
+const renderer = new THREE.WebGLRenderer({ canvas });
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+renderer.render(scene, camera);
