e167e8adc711a1161a9733d02e9239f060c8109e
ameliapham <ptt.huong.8895@gmail.com>
2025-01-18 01:31:58 +0100

    Transform object

--- src/script.tsx
+++ src/script.tsx
@@ -9,6 +9,7 @@ const scene = new THREE.Scene();
 // Set up camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight);
 camera.position.z = 5;
+camera.lookAt(new THREE.Vector3(0, 1, 0))
 
 scene.add(camera);
 
@@ -22,6 +23,14 @@ const geometry = new THREE.BoxGeometry(1,1,1);
 const material = new THREE.MeshBasicMaterial({ color: "pink" });
 
 const mesh = new THREE.Mesh(geometry, material);
+mesh.position.x = 2;
+mesh.position.y = 1;
+
+mesh.scale.set(0.2, -0.5, 2)
+
+mesh.rotation.reorder("YXZ")
+mesh.rotation.y = Math.PI*0.25
+mesh.rotation.x = Math.PI*0.25
 
 scene.add(mesh);
 
