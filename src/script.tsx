920bd7b45924c9c201ed295e5f4b51f581e8a73f
ameliapham <ptt.huong.8895@gmail.com>
2025-04-11 23:58:39 +0200

    Create new objects to understand how to create new object

--- src/script.tsx
+++ src/script.tsx
@@ -9,7 +9,6 @@ const scene = new THREE.Scene();
 // Set up camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight);
 camera.position.z = 5;
-camera.lookAt(new THREE.Vector3(0, 1, 0))
 
 scene.add(camera);
 
@@ -18,21 +17,42 @@ const axesHelper = new THREE.AxesHelper(2);
 
 scene.add(axesHelper);
 
+
 // Create object
 const geometry = new THREE.BoxGeometry(1,1,1);
 const material = new THREE.MeshBasicMaterial({ color: "pink" });
 
-const mesh = new THREE.Mesh(geometry, material);
-mesh.position.x = 2;
-mesh.position.y = 1;
+const mesh1 = new THREE.Mesh(geometry, material);
+mesh1.position.x = 2;
+mesh1.position.y = 1;
 
-mesh.scale.set(0.2, -0.5, 2)
+mesh1.scale.set(0.2, -0.5, 2)
 
-mesh.rotation.reorder("YXZ")
-mesh.rotation.y = Math.PI*0.25
-mesh.rotation.x = Math.PI*0.25
+mesh1.rotation.reorder("YXZ")
+mesh1.rotation.y = Math.PI*0.25
+mesh1.rotation.x = Math.PI*0.25
 
-scene.add(mesh);
+scene.add(mesh1);
+
+
+const mesh2 = new THREE.Mesh(
+    new THREE.ConeGeometry(0.5, 1, 5),
+    new THREE.MeshBasicMaterial({ color: "blue" })
+)
+
+const mesh3 = new THREE.Mesh(
+    new THREE.CircleGeometry(0.5, 105),
+    new THREE.MeshBasicMaterial({ color: "green" })
+)
+
+// Create group object
+const group = new THREE.Group();
+
+scene.add(group);
+
+group.add(mesh1);
+group.add(mesh2);
+group.add(mesh3);
 
 // Set up renderer
 const canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement;
