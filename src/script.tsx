6eee3fb64bc160535e5f0995c163624a77a721d0
ameliapham <ptt.huong.8895@gmail.com>
2025-04-18 01:24:02 +0200

    Using gsap.set with deltaTime

--- src/script.tsx
+++ src/script.tsx
@@ -103,25 +103,30 @@ const renderer = new THREE.WebGLRenderer({ canvas : canvas });
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
+
+// Animation
+let previousTime = performance.now(); 
+/*
 gsap.to(butterfly.rotation, {
     y: "+=" + Math.PI * 2,
     duration: 5,
     ease: "none",
     repeat: -1,
 })
-
-// Animation
-//let previousTime = performance.now(); 
+*/
 
 function animation() {
     //Time 
-    //const currentTime = performance.now()
-    //const deltaTime = ( currentTime - previousTime ) / 1000; // Convert to seconds
-    //previousTime = currentTime;
+    const currentTime = performance.now()
+    const deltaTime = ( currentTime - previousTime ) / 1000; // Convert to seconds
+    previousTime = currentTime;
  
 
     // Update butterfly rotation
     //butterfly.rotation.y += 0.5 * deltaTime;
+    gsap.set(butterfly.rotation, {
+        y: "+=" + 0.5 * deltaTime
+    })
 
     // Render
     renderer.render(scene, camera);
